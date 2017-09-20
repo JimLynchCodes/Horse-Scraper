c632d64aa9573200e6d4530f5ec08d2337b19822
JimTheMan <mrdotjim@gmail.com>
2017-09-19 20:53:43 -0400

    first scrapage

--- scrape.py
+++ scrape.py
@@ -2,22 +2,24 @@ from urllib2 import urlopen
 from bs4 import BeautifulSoup as soup
 import re
 
-myUrl = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38'
+myUrl = 'https://racing.ustrotting.com/waag.aspx'
 print (myUrl)
 
 client = urlopen(myUrl)
 page_html = client.read()
 client.close()
 
+print(page_html);
+
 page_soup = soup(page_html, "html.parser")
-# print(page_html)
+# print(page_soup)
 
 # css_soup
 
-# race_card_links = page_soup.select("a", {"id":".*_lnkbtn"})
-# race_card_links = page_soup.find_all(re.compile("_lnkbtnrace_"));
-# print(race_card_links)
+race_card_links = page_soup.select("a")
+print(race_card_links)
 
-for links in page_soup.select('a'):
-# for links in page_soup.select('a[id*="lnkbtn"]'):
-    print links.get_text()
+for link in page_soup.select('lnkbtnrace'):
+	print links.get_text('Links: ' + link)
+
+	# for links in page_soup.select('a[id*="lnkbtn"]'):
